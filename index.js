e3b35f6239660a1492b398f5df62d50f95a54db9
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-06-04 17:04:39 +0500

    Lecture 4
    1) Switch
    2) Loops
            a) While
            b) For
    3) DOM
    4) debugger
    5) Event Listeners
    6) High level function

--- index.js
+++ index.js
@@ -138,21 +138,21 @@
 // alert("Here are your " + noOfBreads + " breads and here is the change amount: " + buyBread(100, 450));
 
 // Conditional statements
-function calculateMarks () {
-    var marksObtained = Math.random() * 100;
-    marksObtained = Math.floor(marksObtained);
-    marksObtained = marksObtained + 1;
+// function calculateMarks () {
+//     var marksObtained = Math.random() * 100;
+//     marksObtained = Math.floor(marksObtained);
+//     marksObtained = marksObtained + 1;
 
-    if(marksObtained >= 90) {
-        console.log("You are eligible for engineering");
-    } else if(marksObtained < 90 && marksObtained >= 70) {
-        console.log("You have passed");
-    } else {
-        console.log("Better luck next time");
-    }
+//     if(marksObtained >= 90) {
+//         console.log("You are eligible for engineering");
+//     } else if(marksObtained < 90 && marksObtained >= 70) {
+//         console.log("You have passed");
+//     } else {
+//         console.log("Better luck next time");
+//     }
 
-    return marksObtained;
-}
+//     return marksObtained;
+// }
 
 /*
     multiples of 7 you have to say 7up
@@ -165,3 +165,92 @@ function calculateMarks () {
 
 // var cars = ["BMW", "Honda", "Toyota", "Audi"];
 // cars.includes("BMW");
+
+// jahanzeb.naeem@hotmail.com
+// Assignment - fizzbuzz JavaScript
+// Name
+// Batch
+
+/*
+    Today's Objective
+    1) Switch
+    2) Loops
+        a) While
+        b) For
+    3) DOM (Document Object Model)
+    4) debugger
+    5) Event Listeners
+    6) High level function
+*/
+
+// var arrOutput = [];
+// var i = 1;
+
+// function callFizzBuzz() {
+//     while(i<=100) {
+//         if(i%3 === 0 && i%5 === 0) {
+//             arrOutput.push("fizzbuzz")
+//         }
+//         else if(i%3 === 0) {
+//             arrOutput.push("fizz")
+//         } else if(i%5 === 0) {
+//             arrOutput.push("buzz")
+//         } else {
+//             arrOutput.push(i)
+//         }
+//         i++;
+//     }
+//     return arrOutput;
+// }
+
+// var a = 5;
+// var b = "5";
+
+// a == b;
+// ==
+// ===
+// for(var i = 1; i<10; i++) {
+//     console.log(i);
+// }
+
+// Switch
+// switch (new Date().getDay()) {
+//   case 0:
+//     day = "Sunday";
+//     break;
+//   case 1:
+//     day = "Monday";
+//     break;
+//   case 2:
+//      day = "Tuesday";
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     break;
+//   case 4:
+//     day = "Thursday";
+//     break;
+//   case 5:
+//     day = "Friday";
+//     break;
+//   case 6:
+//     day = "Saturday";
+// }
+
+// alert("From external file");
+
+// document.querySelector("#clickMe").addEventListener("click", function(){
+//     alert("this is from an event");
+// });
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function calculate(num1, num2, operator) {
+  return operator(num1, num2);
+}
