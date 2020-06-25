609bb7dfbbe435db780db9049554edc46a578ed0
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-06-25 13:49:02 +0500

    Lecture 9
    Databases, SQL and PHP to MySQL.

--- Inventory/index.php
+++ Inventory/index.php
@@ -28,6 +28,66 @@
   // $toyota = new Toyota("Toyota");
   // echo $toyota->intro();
 
-  greeting::welcome();
+  // greeting::welcome();
+
+  // Procedural DB Connection
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+
+  // Create connection
+  // $conn = mysqli_connect($servername, $username, $password);
+  //
+  // // Check connection
+  // if (!$conn) {
+  //   die("Connection failed: " . mysqli_connect_error());
+  // }
+  // echo "Connected successfully";
+  // mysqli_close($conn);
+
+  // Select Data
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "nexskillfullstackb2php";
+
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+  // Check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+
+  // $sql = "SELECT * FROM Products";
+  $sql = "SELECT Categories.Name CName, Products.Name PName FROM Products INNER JOIN Categories ON Products.CategoryID = Categories.ID WHERE Products.isDeleted != true ";
+  $result = mysqli_query($conn, $sql);
+
+  if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+      // echo "id: " . $row["ID"]. " - Name: " . $row["Name"]. " - Price: " . $row["Price"]. "<br>";
+      echo "Category Name: " . $row["CName"]. " - Product Name: " . $row["PName"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+
+  mysqli_close($conn);
+
+  // OOP DB Connection
+  // $servername = "localhost";
+  // $username = "root";
+  // $password = "";
+  //
+  // // Create connection
+  // $conn = new mysqli($servername, $username, $password);
+  //
+  // // Check connection
+  // if ($conn->connect_error) {
+  //   die("Connection failed: " . $conn->connect_error);
+  // }
+  // echo "Connected successfully with OOP";
+  // $conn->close();
+
   include "dependent/footer.php";
 ?>
